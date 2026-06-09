738e290818482ea43a5700cc8c2a679dbe9d9f64
mythilikannan96ct <mythilikannan96ct@gmail.com>
2026-06-09 21:38:35 +0530

    assignment1

---
+++ assignment1.js
@@ -0,0 +1,17 @@
+let firstName ="mythili";
+let companyName ="test leaf";
+let mobileNumber =7871337107;
+let isAutomation = true;
+let hasplaywright;
+console.log(firstName) ;
+console.log(typeof firstName);
+console.log(companyName);
+console.log(typeof companyName);
+console .log (mobileNumber);
+console.log(typeof mobileNumber);
+console.log(isAutomation) ;
+console.log(typeof isAutomation);
+console.log(hasplaywright);
+console.log(typeof hasplaywright);
+
+
